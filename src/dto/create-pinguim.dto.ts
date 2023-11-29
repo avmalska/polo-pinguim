970d73f1b2f8a233d8c4a3e30316c182fb24d832
Juan Malska <juanandrey2007@hotmail.com>
2023-11-29 14:48:33 -0300

    ajuste swagger

--- src/dto/create-pinguim.dto.ts
+++ src/dto/create-pinguim.dto.ts
@@ -1,11 +1,14 @@
 import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePinguimDto {
+  @ApiProperty({ example: 'Andrey' })
   @IsNotEmpty()
   @IsString()
   @MaxLength(100)
   readonly nome: string;
 
+  @ApiProperty({ example: 10 })
   @IsNotEmpty()
   @IsNumber()
   readonly idade: number;
